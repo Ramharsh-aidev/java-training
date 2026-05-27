be7b532fb993d766ae02789e3b4b2d51c33f1933
Ramharsh-aidev <ramharshdandekar@gmail.com>
2026-05-27 15:12:54 +0530

    add question 5 solution

---
+++ question5/src/main/java/com/ramharsh/question5/entity/Employee.java
@@ -0,0 +1,60 @@
+package com.ramharsh.question5.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double salary, Department department) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
